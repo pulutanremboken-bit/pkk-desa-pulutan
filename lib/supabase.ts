98667cb355a5a6f277b552ffb96bdea124ec531f
pulutanremboken-bit <pulutanremboken@gmail.com>
2026-08-31 17:39:19 +0800

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
 
+let client: ReturnType<typeof createClient> | null = null;
+
 export function supabase() {
-  return createClient(
+  if (client) return client;
+
+  client = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+      },
+    }
   );
+
+  return client;
 }
